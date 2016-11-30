f45aa62ab3dae10406cbc9398dce2788c94ff9d4
Andreas Simon <a.simon@quagilis.de>
2016-11-30 20:31:39 +0100

    Use human-readable module names if possible

--- buildSrc/src/main/groovy/de/quagilis/license/render/AsciidocReportRenderer.groovy
+++ buildSrc/src/main/groovy/de/quagilis/license/render/AsciidocReportRenderer.groovy
@@ -4,10 +4,11 @@ import com.github.jk1.license.ImportedModuleData
 import com.github.jk1.license.LicenseReportPlugin.LicenseReportExtension
 import com.github.jk1.license.ModuleData
 import com.github.jk1.license.ProjectData
+import com.github.jk1.license.render.ReportRenderer
 import org.gradle.api.Project
 
 
-class AsciidocReportRenderer extends com.github.jk1.license.render.SingleInfoReportRenderer {
+class AsciidocReportRenderer implements ReportRenderer {
 
     private String fileName = 'third-party-libraries.adoc'
     private String chapterName = 'Third-party libraries'
@@ -23,7 +24,7 @@ class AsciidocReportRenderer extends com.github.jk1.license.render.SingleInfoRep
         this.chapterName = chapterName
     }
 
-    def void render(ProjectData data) {
+    void render(ProjectData data) {
         project = data.project
         config = project.licenseReport
         output = new File(fileName)
@@ -45,21 +46,22 @@ class AsciidocReportRenderer extends com.github.jk1.license.render.SingleInfoRep
         }
     }
 
-    public String tableHeader() {
+    String tableHeader() {
         "[cols=\"5,2,4\",options=\"header\"]\n" +
         "|===\n" +
         "| Name | Version | Lizenz\n"
     }
 
-    private def void printDependency(ModuleData data) {
-        def moduleName = "${data.group}:${data.name}"
+    private void printDependency(ModuleData data) {
         def moduleVersion = data.version
         def (String moduleUrl, String moduleLicense, String moduleLicenseUrl) = moduleLicenseInfo(config, data)
 
+        output << "| "
+
         if (moduleUrl) {
-            output << "| $moduleUrl[$moduleName]"
+            output << "$moduleUrl[${moduleName(data)}]"
         } else {
-            output << "| $moduleName"
+            output << moduleName(data)
         }
         output << " | $moduleVersion"
         if (moduleLicense) {
@@ -73,7 +75,13 @@ class AsciidocReportRenderer extends com.github.jk1.license.render.SingleInfoRep
         }
     }
 
-    private def void printImportedModule(ImportedModuleData data) {
+    String moduleName(ModuleData data) {
+        def moduleName = data.poms.collect { it.name }.join(', ')
+        if(moduleName.empty) return "${data.group}:${data.name}"
+        return moduleName
+    }
+
+    private void printImportedModule(ImportedModuleData data) {
         if (data.projectUrl) {
             output << "| $data.projectUrl[$data.name]"
         } else {
@@ -89,6 +97,60 @@ class AsciidocReportRenderer extends com.github.jk1.license.render.SingleInfoRep
         } else {
             output << " | *No license information found*\n"
         }
+    }
+
+    protected List<String> moduleLicenseInfo(LicenseReportExtension config, ModuleData data) {
+        String moduleUrl = null
+        String moduleLicense = null
+        String moduleLicenseUrl = null
+
+        data.manifests.each {
+            if (it.url) {
+                moduleUrl = it.url
+            }
+            if (it.license) {
+                if (it.license.startsWith('http')) {
+                    moduleLicenseUrl = it.license
+                }
+                moduleLicense = it.license
+
+            }
+        }
+        data.poms.each {
+            if (it.projectUrl) {
+                moduleUrl = it.projectUrl
+            }
+            if (it.licenses) {
+                it.licenses.each {
+                    if (it.name) {
+                        moduleLicense = it.name
+                    }
+                    if (it.url && it.url.startsWith('http')) {
+                        moduleLicenseUrl = it.url
+                    }
+                }
+            }
+        }
+        if (!moduleLicenseUrl) {
+            data.licenseFiles.each {
+                it.files.each {
+                    def text = new File(config.outputDir, it).text
+                    if (text.contains('Apache License, Version 2.0')) {
+                        moduleLicense = 'Apache License, Version 2.0'
+                        moduleLicenseUrl = 'http://www.apache.org/licenses/LICENSE-2.0'
+                    }
+                    if (text.contains('Apache Software License, Version 1.1')) {
+                        moduleLicense = 'Apache Software License, Version 1.1'
+                        moduleLicenseUrl = 'http://www.apache.org/licenses/LICENSE-1.1'
+                    }
+                    if (text.contains('CDDL')) {
+                        moduleLicense = 'COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Version 1.0'
+                        moduleLicenseUrl = 'http://opensource.org/licenses/CDDL-1.0'
+                    }
+                }
+            }
+        }
+        [moduleUrl, moduleLicense, moduleLicenseUrl]
     }
 
 }
