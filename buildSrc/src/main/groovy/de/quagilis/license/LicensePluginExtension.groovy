e885689cdb919842b1d214481a805ed1ff13e225
Andreas Simon <a.simon@quagilis.de>
2017-05-09 14:06:56 +0200

    Implemented plain license text importer

--- buildSrc/src/main/groovy/de/quagilis/license/LicensePluginExtension.groovy
+++ buildSrc/src/main/groovy/de/quagilis/license/LicensePluginExtension.groovy
@@ -3,17 +3,30 @@ package de.quagilis.license
 
 class LicensePluginExtension {
 
+    public static final String ASCIIDOC_TEXT = 'asciidocText'
+    public static final String PLAIN_TEXT = 'plainText'
+
     boolean failOnForbiddenLicenses = false
 
     final List<License> compliantLicenses = []
 
 
-    void license(Map<String, String> licenseAttributes, Closure matchers) {
+    void license(Map<String, ?> licenseAttributes, Closure matchers) {
         license(new License(
             name: licenseAttributes.name,
             reference: licenseAttributes.reference,
-            licenseText: licenseAttributes.licenseFile.text
+            licenseText: licenseText(licenseAttributes)
         ), matchers)
+    }
+
+    String licenseText(Map<String, ?> licenseAttributes) {
+        if(licenseAttributes.containsKey(ASCIIDOC_TEXT)) {
+            return ((File) licenseAttributes[ASCIIDOC_TEXT]).text
+        }
+        if(licenseAttributes.containsKey(PLAIN_TEXT)) {
+            return "= ${licenseAttributes.name}\n" + ((File) licenseAttributes[PLAIN_TEXT]).text
+        }
+        return ''
     }
 
     void license(License license, Closure matchers) {
